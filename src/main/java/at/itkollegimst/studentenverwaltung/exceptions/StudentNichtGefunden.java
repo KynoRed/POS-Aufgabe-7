851e1fea3b9fc786aa35b6804fd00bc8fe89c507
Samet <redapo881@gmail.com>
2026-01-21 09:00:14 +0100

    Vide: C Datenlayer gemacht!

---
+++ src/main/java/at/itkollegimst/studentenverwaltung/exceptions/StudentNichtGefunden.java
@@ -0,0 +1,9 @@
+package at.itkollegimst.studentenverwaltung.exceptions;
+
+public class StudentNichtGefunden extends Exception {
+
+    public StudentNichtGefunden(String message){
+        super(message);
+    }
+}
+
